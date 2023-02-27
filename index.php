ec54ffb046c16ef4b29cea5dff3ccdb9377e1653
marcobispojr <99209455+marcobispojr@users.noreply.github.com>
2023-02-27 16:46:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primiro Projeto php</title>
+</head>
+   <h1>Ola mundo</h1>
+
+   <h2>3+3=
+   <?php
+echo(3+3)
+   ?> 
+   </h2>
+
+   <h2>1+4=
+   <?php
+echo(1+4)
+   ?> 
+   </h2>
+   
+   <h2>6+7=
+   <?php
+echo(6+7)
+   ?> 
+   </h2>
+   
+   <h2>2+9=
+   <?php
+echo(2+9)
+   ?> 
+   </h2>
+
+ 
+
+<body>
+    
+</body>
+</html>
